af137aaeaf210d314371bd07c501807507c4afd4
pranavec <pranav.ec@gmail.com>
2019-12-25 22:26:07 -0500

    All files added

---
+++ src/ContactServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace Pranavec\Contact;
+    use Illuminate\Support\ServiceProvider;
+
+    class ContactServiceProvider extends ServiceProvider
+    {
+        public function boot(){
+            $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+            $this->loadViewsFrom(__DIR__ . '/views', 'contact');
+        }
+
+        public function register(){
+
+        }
+
+    }
